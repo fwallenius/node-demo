34dbcdc63095004849ef0b3c4cbce9bf1509a67f
Fredrik Wallenius <fredrik.wallenius@gmail.com>
2012-02-26 14:12:08 +0100

    Chat example finished

--- ex2/chat.js
+++ ex2/chat.js
@@ -2,39 +2,48 @@ var allSockets = {};
 var io = null;
 
 exports.setIo = function (_io) {
-	io = _io;	
+    io = _io;	
 };
 
 exports.onSocket = function (socket) {
-	socket.userName = socket.id + "";
-	allSockets["" + socket.id] = socket;
+    socket.userName = socket.id + "";
+    allSockets["" + socket.id] = socket;
 
-	io.sockets.emit('CONNECTED', userInfo());	
-	socket.on('disconnect', onDisconnect);
-	socket.on('CHATMSG', relayChatMessage);
+    io.sockets.emit('CONNECTED', userInfo());	
+    socket.on('disconnect', onDisconnect);
+    socket.on('CHATMSG', relayChatMessage);
+    socket.on('CHANGENAME', nameChange)
 };
 
 var onDisconnect = function () {
-	delete allSockets["" + this.id];
-	io.sockets.emit('CONNECTED', userInfo());	
+    delete allSockets["" + this.id];
+    io.sockets.emit('CONNECTED', userInfo());	
+};
+
+var nameChange = function (data) {
+    allSockets["" + this.id].userName = data.name;
+    io.sockets.emit('CHANGENAME', {
+        userId:     this.id,
+        name:       data.name
+    });
 };
 
 var relayChatMessage = function (data) { 
-	io.sockets.emit('INCMSG', {
-		userId: 	this.id,
-		userName: 	this.userName,
-		message: 	data.text 
-	});	
+    io.sockets.emit('INCMSG', {
+        userId: 	this.id,
+        userName: 	this.userName,
+        message: 	data.text 
+    });	
 };
 
 var userInfo = function () {
-	var data = [], index;
+    var data = [], index;
 
-	for (index in allSockets) {
-		data.push({
-			id: 	allSockets[index].id,
-			name: 	allSockets[index].userName
-		});
-	}
-	return data;
+    for (index in allSockets) {
+        data.push({
+            id: 	allSockets[index].id,
+            name: 	allSockets[index].userName
+        });
+    }
+    return data;
 };
--- ex2/public/javascripts/client.js
+++ ex2/public/javascripts/client.js
@@ -2,36 +2,53 @@
 
 var socket = io.connect();
 
-
 socket.on('CONNECTED', function (data) {
 
-	var html = '', index;
-	for (index in data) {
-		html += '<li>' + data[index].name + '</li>';
-	}
+    var html = '', index;
+    for (index in data) {
+        html += '<li class="' + data[index].id + '">' + data[index].name + '</li>';
+    }
 
-	$('#users').html(html);
+    $('#users').html(html);
 
 });
 
 socket.on('INCMSG', function (data) {
-	var html = '<p><span class="userName ' + data.userId + '">' + data.userName + ': </span> ' + data.message + '</p>';
-	$('#chatMessages').append(html);
-	$('#chatMessages').scrollTop(40000);  // <- ugly hack, but this demo is not about smooth client design
+    var html = '<p><span class="userName ' + data.userId + '">' + data.userName + '</span>: ' + data.message + '</p>';
+    $('#chatMessages').append(html);
+    $('#chatMessages').scrollTop(40000);  // <- ugly hack, but this demo is not about smooth client design
+});
+
+socket.on('CHANGENAME', function (data) {
+    console.log(data);
+    $('.' + data.userId).each(function() {
+        $(this).html(data.name);
+    });
 });
 
 
 var sendMessage = function (el) {
-	socket.emit('CHATMSG', { text: $(el).val() });
-	$(el).val('');
-}
+    socket.emit('CHATMSG', {
+        text: $(el).val()
+    });
+    $(el).val('');
+};
+
+var changeName = function (el) {
+    socket.emit('CHANGENAME', {
+        name: $(el).val()
+    });
+};
 	
 
 $(document).ready(function() {
-	$('#textInput').keyup(function(event) {
-		if (event.keyCode === 13) {
-			sendMessage($('#textInput'));
-		}
-	});	
+    $('#textInput').keyup(function(event) {
+        if (event.keyCode === 13) {
+            sendMessage($('#textInput'));
+        }
+    });
+    $('#nameInput').keyup(function() {
+        changeName($(this)); 
+    });
 });
 
